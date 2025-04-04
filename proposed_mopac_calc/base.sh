23489309e321d8a5c6080aa8560cd80a27724c8b
Mirual <80323799+Mirual@users.noreply.github.com>
2025-04-04 14:13:49 +0300

    Update base.sh

--- proposed_mopac_calc/base.sh
+++ proposed_mopac_calc/base.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #export RSH_COMMAND="/usr/bir/ssh -x"
 #python3 orca_calculate_folder.py
+export OMP_NUM_THREADS=70
 python3 main.py -x opted_h2o.xyz --eps 78.4 --mult SINGLET --charge -1 --step 0.025 --isotope T --moving_atom_h 4 -d 10 -n 0 -t 100 -p /mop/base/calc
