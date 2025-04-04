45406f1ee71dbf5a544d72dcc391135baa07920b
Mirual <80323799+Mirual@users.noreply.github.com>
2025-04-04 14:13:25 +0300

    Update ch4_oh.sh

--- example/ch4_oh.sh
+++ example/ch4_oh.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #export RSH_COMMAND="/usr/bir/ssh -x"
 #python3 orca_calculate_folder.py
+
+export OMP_NUM_THREADS=70
+
 python3 main.py -x opted_h2o.xyz --eps 0 --charge 0 --mult DOUBLET --isotope D --moving_atom_h 1 -d 5 -n 0 -p /mop/ch4_oh/calc
